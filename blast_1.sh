bbb9e88ca54f9e0b004654cd5801ba252558d86e
cmatth25 <101157734+cmatth25@users.noreply.github.com>
2024-07-24 12:28:21 -0400

    Update blast_1.sh

--- blast_1.sh
+++ blast_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --time=00:15:00       # The duration in HH:MM:SS format of each task in the array
-#SBATCH --cpus-per-task=1     # The number of cores for each task in the array
-#SBATCH --mem-per-cpu=100GB    # The memory per core for each task in the array
+#SBATCH --time=00:15:00       
+#SBATCH --cpus-per-task=1     
+#SBATCH --mem-per-cpu=100GB    
 #SBATCH --output=blast_1.%J.out
 #SBATCH --error=blast_1.%J.err
 
