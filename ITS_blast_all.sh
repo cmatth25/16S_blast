c9b1f787ab817c26e176eab943aecd7d07160d93
cmatth25 <101157734+cmatth25@users.noreply.github.com>
2024-07-24 13:03:35 -0400

    Create ITS_blast_all.sh

---
+++ ITS_blast_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=00:15:00       
+#SBATCH --cpus-per-task=1     
+#SBATCH --mem-per-cpu=100GB    
+#SBATCH --output=blast_all.%J.out
+#SBATCH --error=blast_all.%J.err
+
+module load StdEnv/2020 gcc/9.3.0 blast+/2.14.0
+
+blastn -db /cvmfs/bio.data.computecanada.ca/content/databases/Core/blast_dbs/2022_03_23/ITS_RefSeq_Fungi -query \
+        ${1} -outfmt "6 qseqid stitle sallseqid pident evalue" -max_target_seqs 1 | sort -u > ${1::-4}_bl.txt
