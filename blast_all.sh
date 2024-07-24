a0c1b658d42e45f39ce77e0f8af129969a1f2e66
cmatth25 <101157734+cmatth25@users.noreply.github.com>
2024-07-24 11:21:17 -0400

    Update blast_all.sh

--- blast_all.sh
+++ blast_all.sh
@@ -6,6 +6,5 @@
 
 module load StdEnv/2020 gcc/9.3.0 blast+/2.14.0
 
-# Using the index of the current task, given by `$SLURM_ARRAY_TASK_ID`, run the corresponding query and write the result
 blastn -db /cvmfs/bio.data.computecanada.ca/content/databases/Core/blast_dbs/2022_03_23/16S_ribosomal_RNA -query \
         ${1} -outfmt "6 qseqid stitle sallseqid pident evalue" -max_target_seqs 1 | sort -u > ${1::-4}_bl.txt
